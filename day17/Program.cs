cc2d0e8f43736dfae6f24e4bf1548fdfd5a6a582
Andrew <andrewboudreau@gmail.com>
2018-12-18 23:00:11 -0600

    update file organization

---
+++ day17/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+namespace AOC2018.Day17
+{
+	class Program
+	{
+		public static HashSet<Point> Clay = new HashSet<Point>();
+
+		static void Main(string[] args)
+		{
+			ParseInput("example.txt");
+
+			Console.ReadKey();
+		}
+
+		public static IEnumerable<Point> ParseInput(string filePath)
+		{
+			using(var stream = new StreamReader(filePath))
+			{
+				while (!stream.EndOfStream)
+				{
+					var line = stream.ReadLine();
+					Console.WriteLine(line);
+				}
+			}
+
+			return null;
+		}
+	}
+}
---
+++ day5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace solve.day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = React('A', 'a');
+            var foo2 = React('A', 'b');
+
+            var polymer = File.ReadAllText("part1.txt").Trim();
+            var stack = new Stack<char>(polymer.Length);
+
+            foreach (var unit in polymer)
+            {
+               
+            }
+        }
+
+        public static bool React(char poly1, char poly2)
+        {
+            return Math.Abs(poly1 - poly2) == 32;
+        }
+    }
+}
