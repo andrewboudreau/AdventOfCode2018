6c82182de388196c7518cd555a120898d48ce223
Andrew <andrewboudreau@gmail.com>
2018-12-18 23:34:09 -0600

    Update Program.cs

--- day5/Program.cs
+++ day5/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace solve.day5
 {
@@ -9,21 +10,49 @@ namespace solve.day5
     {
         static void Main(string[] args)
         {
-            var foo = React('A', 'a');
-            var foo2 = React('A', 'b');
 
             var polymer = File.ReadAllText("part1.txt").Trim();
-            var stack = new Stack<char>(polymer.Length);
+            var length = CollapsePolymer(polymer);
+            Console.WriteLine($"The remaining polymer has {length} units");
 
-            foreach (var unit in polymer)
+            var min = int.MaxValue;
+            for (var chr = 'a'; chr <= 'z'; chr++)
             {
-               
+                var poly = polymer.Replace(chr.ToString(), string.Empty, StringComparison.OrdinalIgnoreCase);
+                var collapsedLength = CollapsePolymer(poly);
+                Console.WriteLine($"Removing '{chr.ToString()}/{chr.ToString().ToUpper()}' gives {collapsedLength} units");
+
+                min = Math.Min(min, collapsedLength);
             }
+
+            Console.WriteLine($"The shortest polymer that could be made is {min} units");
+
+            Console.ReadKey();
         }
 
-        public static bool React(char poly1, char poly2)
+        public static int CollapsePolymer(string polymer)
         {
-            return Math.Abs(poly1 - poly2) == 32;
+            // check if the polymer units are the magic number away from each other on the ascii table.
+            Func<int, int, bool> Reacts = (poly1, poly2) => Math.Abs(poly1 - poly2) == 32;
+
+            var stack = new Stack<char>(polymer.Length);
+            foreach (var unit in polymer)
+            {
+                if (stack.TryPop(out var adjacent))
+                {
+                    if (!Reacts(unit, adjacent))
+                    {
+                        stack.Push(adjacent);
+                        stack.Push(unit);
+                    }
+                }
+                else
+                {
+                    stack.Push(unit);
+                }
+            }
+
+            return stack.Count;
         }
     }
 }
