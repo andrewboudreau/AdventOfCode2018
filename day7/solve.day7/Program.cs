a4bfe2f86c81ad42dbac5b0addfa3559ee82817e
Andrew <andrewboudreau@gmail.com>
2018-12-21 21:36:30 -0600

    Update Program.cs

--- day7/solve.day7/Program.cs
+++ day7/solve.day7/Program.cs
@@ -7,137 +7,148 @@ using System.Text.RegularExpressions;
 
 namespace solve.day7
 {
-	class Program
-	{
-		// https://en.wikipedia.org/wiki/Topological_sorting
-		static void Main(string[] args)
-		{
-			var input = ParseInput("example.txt");
-			//var input = ParseInput("part1.txt");
+    class Program
+    {
+        // https://en.wikipedia.org/wiki/Topological_sorting
+        static void Main(string[] args)
+        {
+            var input = ParseInput("example.txt");
+            //var input = ParseInput("part1.txt");
 
-			var steps = new SortedSet<Step>();
+            var steps = new SortedSet<Step>();
 
-			foreach (var (Parent, Child) in input)
-			{
-				var parent = new Step(Parent);
-				var child = new Step(Child);
+            foreach (var (Parent, Child) in input)
+            {
+                var parent = new Step(Parent);
+                var child = new Step(Child);
 
-				steps.Add(parent);
-				steps.Add(child);
+                if (!steps.Contains(parent))
+                {
+                    steps.Add(parent);
+                }
+                else
+                {
 
-				parent.AddChild(child);
-				child.AddParent(parent);
+                }
+                steps.Add(child);
 
-				//if (steps.All(x => x.Value != Parent))
-				//{
-				//	var step = new Step(Parent);
-				//	step.AddChild(new Step(Child));
-				//	steps.Add(step);
-				//}
-				//else
-				//{
-				//	var step = steps.Single(x => x.Value == Parent);
-				//	step.AddChild(new Step(Child));
-				//}
-			}
+                parent.AddChild(child);
+                child.AddParent(parent);
 
-			//noDependency = new SortedSet<char>(noDependency.Except(hasDependency));
-			//Console.WriteLine($"No Dependencies: {string.Join(", ", noDependency)}");
+                //if (steps.All(x => x.Value != Parent))
+                //{
+                //	var step = new Step(Parent);
+                //	step.AddChild(new Step(Child));
+                //	steps.Add(step);
+                //}
+                //else
+                //{
+                //	var step = steps.Single(x => x.Value == Parent);
+                //	step.AddChild(new Step(Child));
+                //}
+            }
 
-			//while (noDependency.Any())
-			//{
+            //noDependency = new SortedSet<char>(noDependency.Except(hasDependency));
+            //Console.WriteLine($"No Dependencies: {string.Join(", ", noDependency)}");
 
-			//}
-			foreach (var step in steps)
-			{
-				Console.WriteLine($"{step.Value}");
-			}
-			Console.WriteLine("Done, press any key to exit.");
-			Console.ReadKey();
+            //while (noDependency.Any())
+            //{
 
-		}
+            //}
+            foreach (var step in steps)
+            {
+                Console.WriteLine($"{step}");
+            }
+            Console.WriteLine("Done, press any key to exit.");
+            Console.ReadKey();
 
-		public static IEnumerable<(char StepOne, char StepTwo)> ParseInput(string filePath)
-		{
-			var regex = new Regex(@"\b[A-Z]\b");
-			foreach (var line in File.ReadAllLines(filePath))
-			{
-				var matches = regex.Matches(line);
-				yield return (matches[0].Value[0], matches[1].Value[0]);
-			}
-		}
+        }
 
-		public class Step : IComparable<Step>
-		{
-			public Step(char value)
-			{
-				Value = value;
-			}
+        public static IEnumerable<(char StepOne, char StepTwo)> ParseInput(string filePath)
+        {
+            var regex = new Regex(@"\b[A-Z]\b");
+            foreach (var line in File.ReadAllLines(filePath))
+            {
+                var matches = regex.Matches(line);
+                yield return (matches[0].Value[0], matches[1].Value[0]);
+            }
+        }
 
-			public char Value { get; private set; }
+        public class Step : IComparable<Step>
+        {
+            public Step(char value)
+            {
+                Value = value;
+            }
 
-			public List<Step> Parents { get; private set; } = new List<Step>();
+            public char Value { get; private set; }
 
-			public List<Step> Children { get; private set; } = new List<Step>();
+            public List<Step> Parents { get; private set; } = new List<Step>();
 
-			public void AddParent(Step dependency)
-			{
-				Parents.Add(dependency);
-			}
+            public List<Step> Children { get; private set; } = new List<Step>();
 
-			public void AddChild(Step child)
-			{
-				Children.Add(child);
-			}
+            public void AddParent(Step dependency)
+            {
+                Parents.Add(dependency);
+            }
 
-			public override string ToString()
-			{
-				var sb = new StringBuilder();
-				if (Parents.Any())
-				{
-					sb.Append($"({string.Join(", ", Parents.Select(x => x.Value))} -> ");
-				}
+            public void AddChild(Step child)
+            {
+                Children.Add(child);
+            }
 
-				sb.Append($"{Value}");
+            public override string ToString()
+            {
+                var sb = new StringBuilder();
+                if (Parents.Any())
+                {
+                    sb.Append($"({string.Join(", ", Parents.Select(x => x.Value))}) -> ");
+                }
 
-				if (Children.Any())
-				{
-					sb.Append($" -> {string.Join(", ", Children.Select(x => x.Value))})");
-				}
+                sb.Append($"{Value}");
 
-				return sb.ToString();
-			}
+                if (Children.Any())
+                {
+                    sb.Append($" -> ({string.Join(", ", Children.Select(x => x.Value))})");
+                }
 
-			public override bool Equals(object obj)
-			{
-				if (obj == null)
-				{
-					return false;
-				}
+                return sb.ToString();
+            }
 
-				var otherStep = obj as Step;
-				if (otherStep == null)
-				{
-					return false;
-				}
+            public override bool Equals(object obj)
+            {
+                if (obj == null)
+                {
+                    return false;
+                }
 
-				return Value == otherStep.Value;
-			}
+                var otherStep = obj as Step;
+                if (otherStep == null)
+                {
+                    return false;
+                }
 
-			public override int GetHashCode()
-			{
-				return Value.GetHashCode();
-			}
+                return Value == otherStep.Value;
+            }
 
-			public int CompareTo(Step other)
-			{
-				if (other == null)
-				{
-					return 1;
-				}
+            public override int GetHashCode()
+            {
+                return Value.GetHashCode();
+            }
 
-				return Value.CompareTo(other.Value);
-			}
-		}
-	}
+            public int CompareTo(Step other)
+            {
+                if (other == null)
+                {
+                    return 1;
+                }
+
+                return Value.CompareTo(other.Value);
+            }
+        }
+
+        public static class StepExtensions
+        {
+        }
+    }
 }
